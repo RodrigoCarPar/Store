70d5f87accb2798ef23d1236a6ac9d332d1e9625
RodrigoCarPar <rodrigocarrionparada2000@gmail.com>
2024-07-08 12:38:14 +0200

    Store
    Sin Modificar UI

---
+++ Store/src/store/Store.java
@@ -0,0 +1,13 @@
+package store;
+
+public class Store {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login ventana1 = new Login();        
+    }
+    
+}
